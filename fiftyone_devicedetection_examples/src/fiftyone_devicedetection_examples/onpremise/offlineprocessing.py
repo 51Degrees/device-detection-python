8e4a99e7df355eb354aa2bb22ceda98fcc723f8e
Bohdan <25197509+BohdanVV@users.noreply.github.com>
2025-05-21 19:48:27 +0200

    Update offlineprocessing.py

--- fiftyone_devicedetection_examples/src/fiftyone_devicedetection_examples/onpremise/offlineprocessing.py
+++ fiftyone_devicedetection_examples/src/fiftyone_devicedetection_examples/onpremise/offlineprocessing.py
@@ -42,7 +42,7 @@
 # performance and predictive power using Performance Profile, Graph and Difference and Drift 
 # settings.
 # 
-# This example is available in full on [GitHub](https://github.com/51Degrees/device-detection-python/blob/main/fiftyone_devicedetection_examples/fiftyone_devicedetection_examples/onpremise/offlineprocessing.py). 
+# This example is available in full on [GitHub](https://github.com/51Degrees/device-detection-python/blob/main/fiftyone_devicedetection_examples/src/fiftyone_devicedetection_examples/onpremise/offlineprocessing.py). 
 # 
 # @include{doc} example-require-datafile.txt
 # 
@@ -73,15 +73,15 @@ class OfflineProcessing():
         # In this example, we use the DeviceDetectionPipelineBuilder
         # and configure it in code. For more information about
         # pipelines in general see the documentation at
-        # http://51degrees.com/documentation/4.3/_concepts__configuration__builders__index.html
+        # https://51degrees.com/documentation/_concepts__configuration__builders__index.html
         pipeline = DeviceDetectionPipelineBuilder(
             data_file_path = data_file,
             # We use the low memory profile as its performance is
             # sufficient for this example. See the documentation for
             # more detail on this and other configuration options:
-            # http://51degrees.com/documentation/4.3/_device_detection__features__performance_options.html
-            # http://51degrees.com/documentation/4.3/_features__automatic_datafile_updates.html
-            # http://51degrees.com/documentation/4.3/_features__usage_sharing.html
+            # https://51degrees.com/documentation/_device_detection__features__performance_options.html
+            # https://51degrees.com/documentation/_features__automatic_datafile_updates.html
+            # https://51degrees.com/documentation/_features__usage_sharing.html
             performance_profile = "LowMemory",
             # inhibit sharing usage for this test, usually this
             # should be set "true"
